ddf8ad7e1ccc995e53e01216d7427a03dca8c4c6
Justin <justinbfigueroa@gmail.com>
2021-06-27 07:13:47 -0700

    Base locale strings and reference class

---
+++ src/main/java/dev/justinf/infinitywarps/locale/IWRefs.java
@@ -0,0 +1,15 @@
+package dev.justinf.infinitywarps.locale;
+
+public class IWRefs {
+
+    public static final String GENERAL_PREFIX = "infinitywarps.general.prefix";
+    public static final String GENERAL_NO_PERMISSION = "infinitywarps.general.no-permission";
+    public static final String GENERAL_MUST_BE_PLAYER = "infinitywarps.general.must-be-player";
+    public static final String GENERAL_TO_BE_IMPLEMENTED = "infinitywarps.general.to-be-implemented";
+    public static final String COMMAND_IW_HELP_MESSAGE = "infinitywarps.command.iw.help.message";
+    public static final String COMMAND_LISTWARPS_NO_WARPS = "infinitywarps.command.listwarps.no-warps";
+    public static final String COMMAND_LISTWARPS_INVALID_PAGE = "infinitywarps.command.listwarps.invalid-page";
+    public static final String COMMAND_WARP_USAGE = "infinitywarps.command.warp.usage";
+    public static final String COMMAND_WARP_INVALID_WARP = "infinitywarps.command.warp.invalid-warp";
+    public static final String PANEL_TITLE_WARPS = "infinitywarps.panel.title.warps";
+}
